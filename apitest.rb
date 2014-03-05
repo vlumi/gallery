84dc854ce1ae95582699fd86e476d0a3beabee84
Ville Misaki <ville@misaki.fi>
2014-03-05 15:47:43 +0200

    Run through Ruby Beautify.

--- apitest.rb
+++ apitest.rb
@@ -9,15 +9,15 @@ gallery = Gallery::Gallery.new("gallery.db", filters)
 
 count = 0
 gallery.getYears().each do |y|
-	puts y
-	gallery.getMonths(y).each do |m|
-		puts " - #{m}"
-		gallery.getDays(y, m).each do |d|
-			print "   - #{d}: #{gallery.getPhotos(y, m, d).length}; "
-			count = count + gallery.getPhotos(y, m, d).length
-			puts gallery.getPhotos(y, m, d).join(', ')
-		end
-	end
+  puts y
+  gallery.getMonths(y).each do |m|
+    puts " - #{m}"
+    gallery.getDays(y, m).each do |d|
+      print "   - #{d}: #{gallery.getPhotos(y, m, d).length}; "
+      count = count + gallery.getPhotos(y, m, d).length
+      puts gallery.getPhotos(y, m, d).join(', ')
+    end
+  end
 end
 
 puts "Total count: #{count}"
@@ -25,17 +25,17 @@ puts
 puts "Countries:"
 countries = gallery.getCountries()
 countries.keys.each do |c|
-	puts " - #{c} = #{countries[c]}"
+  puts " - #{c} = #{countries[c]}"
 end
 puts
 puts "Cameras:"
 cameras = gallery.getCameras()
 cameras.each do |c|
-	puts " - #{c}"
+  puts " - #{c}"
 end
 puts
 puts "Authors:"
 authors = gallery.getAuthors()
 authors.each do |c|
-	puts " - #{c}"
+  puts " - #{c}"
 end
