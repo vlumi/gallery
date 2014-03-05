5c8cf6e3a1d42501efdc97e36c9a57c071d83638
Ville Misaki <ville@misaki.fi>
2014-03-05 16:25:57 +0200

    Add script for setting the access rights to the files and directories.

---
+++ fix_rights.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+chmod 700 *.rb *.sh full/ .git/
+chmod 600 .gitignore schema.sql TODO.txt
+
+chmod 755 css/ i/ images/ js/ rb/ thumbs/
+chmod 644 css/* gallery.db i/* images/* index.rhtml js/* rb/* thumbs/*
