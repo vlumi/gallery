2592ddb3a40de841c5f9f1cf892bc45c440e4b55
Ville Misaki <ville@misaki.fi>
2014-03-08 17:28:11 +0200

    Update to match the current set of files.

--- fix_rights.sh
+++ fix_rights.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 chmod 700 *.rb *.sh full/ .git/
-chmod 600 .gitignore schema.sql TODO.txt
+chmod 600 .gitignore *.ddl TODO.txt
 
 chmod 755 css/ i/ images/ js/ rb/ thumbs/
-chmod 644 css/* gallery.db i/* images/* index.rhtml js/* rb/* thumbs/*
+chmod 644 css/* *.sqlite3 i/* images/* index.rhtml js/* rb/* thumbs/*
