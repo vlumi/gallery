4a89bc08496c1237342ad2a458b28739c75b6daa
Ville Misaki <ville@misaki.fi>
2014-03-08 21:49:05 +0200

    Fixes to directory rights and temporary files.

--- fix_rights.sh
+++ fix_rights.sh
@@ -3,5 +3,5 @@
 chmod 700 *.rb *.sh full/ .git/
 chmod 600 .gitignore *.ddl TODO.txt
 
-chmod 755 css/ i/ images/ js/ rb/ thumbs/
+chmod 755 . css/ i/ images/ js/ rb/ thumbs/
 chmod 644 css/* *.sqlite3 i/* images/* index.rhtml js/* rb/* thumbs/*
