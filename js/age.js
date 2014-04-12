fe9bd0e20feb7a8a012738f3adb707af47e984d6
Ville Misaki <ville@misaki.fi>
2014-04-12 13:55:17 +0300

    Refactor javascripts to be within a single vlumi namespace

--- js/age.js
+++ js/age.js
@@ -1,4 +1,4 @@
-// Copyright 2012 Ville Misaki <ville@misaki.fi>. All rights reserved.
+// Copyright 2012-2014 Ville Misaki <ville@misaki.fi>. All rights reserved.
 
 // A function for creating a string representation for the difference of two Date objects.
 // Outputs the time difference in years, months, weeks and days. Optionally can also
@@ -21,232 +21,238 @@
 //     var t = setInterval(update_age, 1000); // Update once a second
 // ...
 //     <p> Age: <span id="age">(calculating...)</span> </p>
-function getDateDiffStr(p_startd, p_endd, format, precision) {
-    var startd = new Date(p_startd);
-    var endd = new Date(p_endd);
+if (typeof vlumi === 'undefined')
+    vlumi = {};
 
-	// Maps for constructing the date string.
-	// These may be localized if needed.
-	var partTitlesSingular = [' year', ' month', ' week', ' day', ' hour', ' minute', ' second'];
-	var partTitlesPlural = [' years', ' months', ' weeks', ' days', ' hours', ' minutes', ' seconds'];
-	var partTitlesShort = ['歳', 'ヶ月', '週', '日', '', '', ''];
-	var timeSeparator = ':';
-	
-	if (startd > endd) {
-		var tempd = startd;
-		startd = endd;
-		endd = tempd;
-	}
-	
-	var retstr = '';
-	
-	if (format == 'full' || format == 'full_short' || format == 'date' || format == 'date_short') {
-        var is_short = false;
-        if (format == 'full_short' || format == 'date_short') {
-            is_short = true;
+vlumi.Age = {
+    getDateDiffStr: function(p_startd, p_endd, format, precision) {
+        var startd = new Date(p_startd);
+        var endd = new Date(p_endd);
+
+        // Maps for constructing the date string.
+        // These may be localized if needed.
+        var partTitlesSingular = [' year', ' month', ' week', ' day', ' hour', ' minute', ' second'];
+        var partTitlesPlural = [' years', ' months', ' weeks', ' days', ' hours', ' minutes', ' seconds'];
+        var partTitlesShort = ['歳', 'ヶ月', '週', '日', '', '', ''];
+        var timeSeparator = ':';
+        
+        if (startd > endd) {
+            var tempd = startd;
+            startd = endd;
+            endd = tempd;
         }
-		// Calculate full years and months; variable length, so needs looping.
-		var years = -1;
-		var months = -1;
-		while (startd < endd) {
-			startd.setUTCFullYear(startd.getUTCFullYear() + 1);
-			years++;
-		}
-		startd.setUTCFullYear(startd.getUTCFullYear() - 1);
-		while (startd < endd) {
-			startd.setUTCMonth(startd.getUTCMonth() + 1);
-			months++;
-		}
-		startd.setUTCMonth(startd.getUTCMonth() - 1);
-		
-		var dateParts = [];
-		var timeParts = [];
-		
-		// Moved to within a month; calculate the rest from the millisecond difference.
-		var t = endd - startd;
-		var weeks = Math.floor((t / (1000 * 60 * 60 * 24 * 7)));
-		var days = Math.floor((t / (1000 * 60 * 60 * 24)) % 7);
-		
-		// Collect the date string parts.
-		var diffDateParts = [years, months, weeks, days];
-		for (var i = 0; i < diffDateParts.length; i++) {
-			var val = diffDateParts[i];
-			if (val == 0 && (format == 'full' || format == 'full_short' || dateParts.length > 0 || i < 3)) {
-                continue;
-			}
-			var title = (is_short
-                ? partTitlesShort[i]
-                : (val == 1 ? partTitlesSingular[i] : partTitlesPlural[i]));
-			dateParts.push(format_bignum(val, ',', 0) + title);
-		}
-		if (dateParts.length == 0 && format == 'date') {
-			// Default to "0 days"
-			dateParts.push(0 + partTitlesPlural[3]);
-		}
-		
-		if (format == 'full' || format == 'full_short') {
-			var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-			var minutes = Math.floor((t / (1000 * 60)) % 60);
-			var seconds = Math.floor((t / 1000) % 60);
-			
-			// Collect the time string parts.
-			var diffTimeParts = [hours, minutes, seconds];
-			for (var i = 0; i < diffTimeParts.length; i++) {
-                var val = diffTimeParts[i];
-                if (val < 10) {
-                    val = '0' + val;
+        
+        var retstr = '';
+        
+        if (format == 'full' || format == 'full_short' || format == 'date' || format == 'date_short') {
+            var is_short = false;
+            if (format == 'full_short' || format == 'date_short') {
+                is_short = true;
+            }
+            // Calculate full years and months; variable length, so needs looping.
+            var years = -1;
+            var months = -1;
+            while (startd < endd) {
+                startd.setUTCFullYear(startd.getUTCFullYear() + 1);
+                years++;
+            }
+            startd.setUTCFullYear(startd.getUTCFullYear() - 1);
+            while (startd < endd) {
+                startd.setUTCMonth(startd.getUTCMonth() + 1);
+                months++;
+            }
+            startd.setUTCMonth(startd.getUTCMonth() - 1);
+            
+            var dateParts = [];
+            var timeParts = [];
+            
+            // Moved to within a month; calculate the rest from the millisecond difference.
+            var t = endd - startd;
+            var weeks = Math.floor((t / (1000 * 60 * 60 * 24 * 7)));
+            var days = Math.floor((t / (1000 * 60 * 60 * 24)) % 7);
+            
+            // Collect the date string parts.
+            var diffDateParts = [years, months, weeks, days];
+            for (var i = 0; i < diffDateParts.length; i++) {
+                var val = diffDateParts[i];
+                if (val == 0 && (format == 'full' || format == 'full_short' || dateParts.length > 0 || i < 3)) {
+                    continue;
                 }
-                timeParts.push(val);
-			}
-		}
-		
-		// Join the parts together.
-		var strParts = [];
-		
-		if (is_short) {
-			strParts.push(dateParts.join('<br/>'));
-		}
-		else if (dateParts.length > 1) {
-			var lastDatePart = dateParts.pop();
-			strParts.push([dateParts.join(', '), lastDatePart].join(' and '));
-		}
-		else {
-			strParts.push(dateParts.join(', '));
-		}
-		
-		if (format == 'full' || format == 'full_short') {
-			strParts.push(timeParts.join(timeSeparator));
-		}
-		if (is_short) {
-            retstr = strParts.join('<br/>');
-		}
-		else {
-            retstr = strParts.join(', ');
+                var title = (is_short
+                             ? partTitlesShort[i]
+                             : (val == 1 ? partTitlesSingular[i] : partTitlesPlural[i]));
+                dateParts.push(vlumi.Age.format_bignum(val, ',', 0) + title);
+            }
+            if (dateParts.length == 0 && format == 'date') {
+                // Default to "0 days"
+                dateParts.push(0 + partTitlesPlural[3]);
+            }
+            
+            if (format == 'full' || format == 'full_short') {
+                var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+                var minutes = Math.floor((t / (1000 * 60)) % 60);
+                var seconds = Math.floor((t / 1000) % 60);
+                
+                // Collect the time string parts.
+                var diffTimeParts = [hours, minutes, seconds];
+                for (var i = 0; i < diffTimeParts.length; i++) {
+                    var val = diffTimeParts[i];
+                    if (val < 10) {
+                        val = '0' + val;
+                    }
+                    timeParts.push(val);
+                }
+            }
+            
+            // Join the parts together.
+            var strParts = [];
+            
+            if (is_short) {
+                strParts.push(dateParts.join('<br/>'));
+            }
+            else if (dateParts.length > 1) {
+                var lastDatePart = dateParts.pop();
+                strParts.push([dateParts.join(', '), lastDatePart].join(' and '));
+            }
+            else {
+                strParts.push(dateParts.join(', '));
+            }
+            
+            if (format == 'full' || format == 'full_short') {
+                strParts.push(timeParts.join(timeSeparator));
+            }
+            if (is_short) {
+                retstr = strParts.join('<br/>');
+            }
+            else {
+                retstr = strParts.join(', ');
+            }
         }
-	}
-	else {
-		var val = '';
-		var title_idx = -1;
-		
-		if (format == 'years') {
-			// Years, taking leap years into account.
-			
-			// Count the number of full years by looping.
-			var years = -1;
-			while (startd < endd) {
-				startd.setUTCFullYear(startd.getUTCFullYear() + 1);
-				years++;
-			}
-			var t_left = startd - endd;
-			startd.setUTCFullYear(startd.getUTCFullYear() - 1);
-			
-			// Fractions from what's left after looping.
-			var t_gone = endd - startd;
-			years += t_gone / (t_gone + t_left);
-			
-			val = years;
-			title_idx = 0;
-			
-			var title = (val == 1 ? partTitlesSingular[title_idx] : partTitlesPlural[title_idx]);
-			
-			var mul = Math.pow(10, precision);
-			val = Math.floor(val * mul) / mul;
-		}
-		else if (format == 'months') {
-			// Full months.
-			var months = -1;
-			while (startd < endd) {
-				startd.setUTCMonth(startd.getUTCMonth() + 1);
-				months++;
-			}
-			var t_left = startd - endd;
-			startd.setUTCMonth(startd.getUTCMonth() - 1);
-			
-			// Fractions from what's left after looping.
-			var t_gone = endd - startd;
-			months += t_gone / (t_gone + t_left);
-			
-			val = months;
-			title_idx = 1;
-		}
-		else {
-			// Full time difference, in milliseconds
-			var t = endd - startd;
-			
-			var val = '';
-			if (format == 'weeks') {
-				val = t / (1000 * 60 * 60 * 24 * 7);
-				title_idx = 2;
-			}
-			else if (format == 'days') {
-				val = t / (1000 * 60 * 60 * 24);
-				title_idx = 3;
-			}
-			else if (format == 'hours') {
-				val = t / (1000 * 60 * 60);
-				title_idx = 4;
-			}
-			else if (format == 'minutes') {
-				val = t / (1000 * 60);
-				title_idx = 5;
-			}
-			else if (format == 'seconds') {
-				val = t / 1000;
-				title_idx = 6;
-			}
-		}
-		var title = (val == 1 ? partTitlesSingular[title_idx] : partTitlesPlural[title_idx]);
-		
-		val = format_bignum(val, ',', precision);
-		
-		retstr = val + title;
-	}
-	
-	return retstr;
-}
+        else {
+            var val = '';
+            var title_idx = -1;
+            
+            if (format == 'years') {
+                // Years, taking leap years into account.
+                
+                // Count the number of full years by looping.
+                var years = -1;
+                while (startd < endd) {
+                    startd.setUTCFullYear(startd.getUTCFullYear() + 1);
+                    years++;
+                }
+                var t_left = startd - endd;
+                startd.setUTCFullYear(startd.getUTCFullYear() - 1);
+                
+                // Fractions from what's left after looping.
+                var t_gone = endd - startd;
+                years += t_gone / (t_gone + t_left);
+                
+                val = years;
+                title_idx = 0;
+                
+                var title = (val == 1 ? partTitlesSingular[title_idx] : partTitlesPlural[title_idx]);
+                
+                var mul = Math.pow(10, precision);
+                val = Math.floor(val * mul) / mul;
+            }
+            else if (format == 'months') {
+                // Full months.
+                var months = -1;
+                while (startd < endd) {
+                    startd.setUTCMonth(startd.getUTCMonth() + 1);
+                    months++;
+                }
+                var t_left = startd - endd;
+                startd.setUTCMonth(startd.getUTCMonth() - 1);
+                
+                // Fractions from what's left after looping.
+                var t_gone = endd - startd;
+                months += t_gone / (t_gone + t_left);
+                
+                val = months;
+                title_idx = 1;
+            }
+            else {
+                // Full time difference, in milliseconds
+                var t = endd - startd;
+                
+                var val = '';
+                if (format == 'weeks') {
+                    val = t / (1000 * 60 * 60 * 24 * 7);
+                    title_idx = 2;
+                }
+                else if (format == 'days') {
+                    val = t / (1000 * 60 * 60 * 24);
+                    title_idx = 3;
+                }
+                else if (format == 'hours') {
+                    val = t / (1000 * 60 * 60);
+                    title_idx = 4;
+                }
+                else if (format == 'minutes') {
+                    val = t / (1000 * 60);
+                    title_idx = 5;
+                }
+                else if (format == 'seconds') {
+                    val = t / 1000;
+                    title_idx = 6;
+                }
+            }
+            var title = (val == 1 ? partTitlesSingular[title_idx] : partTitlesPlural[title_idx]);
+            
+            val = vlumi.Age.format_bignum(val, ',', precision);
+            
+            retstr = val + title;
+        }
+        
+        return retstr;
+    },
 
-// Display the number with thousand-separators, with exactly <precision> decimals.
-function format_bignum(num, sep, precision) {
-	// Split the number into parts.
-	var parts = [];
-	do {
-		var val = num % 1000;
-		if (num > 1000) {
-			// Padding if in the middle of the number
-			if (val < 10) {
-				val = "00" + val;
-			}
-			else if (val < 100) {
-				val = "0" + val;
-			}
-		}
-		parts.unshift(val);
-		num = Math.floor(num / 1000);
-	} while (num > 0);
-	
-	// Collect the parts as a thousand-separated string
-	var retstr = '0';
-	if (parts.length > 0) {
-		retstr = parts.join(sep);
-	}
+    // Display the number with thousand-separators, with exactly <precision> decimals.
+    format_bignum: function(num, sep, precision) {
+        // Split the number into parts.
+        var parts = [];
+        do {
+            var val = num % 1000;
+            if (num > 1000) {
+                // Padding if in the middle of the number
+                if (val < 10) {
+                    val = "00" + val;
+                }
+                else if (val < 100) {
+                    val = "0" + val;
+                }
+            }
+            parts.unshift(val);
+            num = Math.floor(num / 1000);
+        } while (num > 0);
+        
+        // Collect the parts as a thousand-separated string
+        var retstr = '0';
+        if (parts.length > 0) {
+            retstr = parts.join(sep);
+        }
 
-	// Always rounding down, not to reach milestones early.
-    parts = retstr.split('.', 2)
-    retstr = parts[0];
-    if (precision > 0) {
-    	if (parts[1] === undefined) {
-    		parts[1] = '0';
-    	}
-    	if (parts[1].length > precision) {
-    		parts[1] = parts[1].substring(0, precision);
-    	}
-    	else {
-    		while (parts[1].length < precision) {
-    			parts[1] += '0';
-    		}
-    	}
-    	retstr += '.' + parts[1];
-    }
-	
-	return retstr
-}
+        // Always rounding down, not to reach milestones early.
+        parts = retstr.split('.', 2)
+        retstr = parts[0];
+        if (precision > 0) {
+            if (parts[1] === undefined) {
+                parts[1] = '0';
+            }
+            if (parts[1].length > precision) {
+                parts[1] = parts[1].substring(0, precision);
+            }
+            else {
+                while (parts[1].length < precision) {
+                    parts[1] += '0';
+                }
+            }
+            retstr += '.' + parts[1];
+        }
+        
+        return retstr
+    },
+};
+
--- js/gallery.js
+++ js/gallery.js
@@ -1,122 +1,127 @@
-// Copyright 2013 Ville Misaki <ville@misaki.fi>. All right reserved.
+// Copyright 2013-2014 Ville Misaki <ville@misaki.fi>. All right reserved.
 
-function show_month(id) {
-	if (id == 'first') {
-		id = $('#months').children().first().attr('id')
-	}
-	else if (id == 'last') {
-		id = $('#months').children().last().attr('id')
-	}
-	if (! $("#" + id).is(":visible")) {
-		var curr_month = get_curr_month();
-		$("div.month_block").hide();
-		
-		var title = $("#" + id).attr("title");
-		$(".month_nav span.title").html(title);
-		
-		// Hide navigation links if first/last month.
-		var prev_month = $("#" + id).prev();
-		if (prev_month.length == 0) {
-			$(".month_nav span.first").css("visibility", "hidden");
-			$(".month_nav span.prev").css("visibility", "hidden");
-		}
-		else {
-			$(".month_nav span.first").css("visibility", "");
-			$(".month_nav span.prev").css("visibility", "").attr("title", $(prev_month).attr("title"));
-		}
-		var next_month = $("#" + id).next();
-		if (next_month.length == 0) {
-			$(".month_nav span.next").css("visibility", "hidden");
-			$(".month_nav span.last").css("visibility", "hidden");
-			if (curr_month != null && history != null && typeof history.pushState == 'function') {
-				var filters = get_url_filters();
-                history.pushState(null, 'Lenni', window.location.pathname + (filters.length > 0 ? '?' + filters : ''));
+if (typeof vlumi === 'undefined')
+    vlumi = {};
+
+vlumi.Gallery = {
+    show_month: function(id) {
+        if (id == 'first') {
+            id = $('#months').children().first().attr('id')
+        }
+        else if (id == 'last') {
+            id = $('#months').children().last().attr('id')
+        }
+        if (! $("#" + id).is(":visible")) {
+            var curr_month = vlumi.Gallery.get_curr_month();
+            $("div.month_block").hide();
+            
+            var title = $("#" + id).attr("title");
+            $(".month_nav span.title").html(title);
+            
+            // Hide navigation links if first/last month.
+            var prev_month = $("#" + id).prev();
+            if (prev_month.length == 0) {
+                $(".month_nav span.first").css("visibility", "hidden");
+                $(".month_nav span.prev").css("visibility", "hidden");
             }
-		}
-		else {
-			$(".month_nav span.next").css("visibility", "").attr("title", $(next_month).attr("title"));
-			$(".month_nav span.last").css("visibility", "");
-			if (curr_month != null && history != null && typeof history.pushState == 'function') {
-				var filters = get_url_filters();
-                history.pushState(null, 'Lenni', window.location.pathname + '?m=' + id.substr(1) + (filters.length > 0 ? '&' + filters : ''));
+            else {
+                $(".month_nav span.first").css("visibility", "");
+                $(".month_nav span.prev").css("visibility", "").attr("title", $(prev_month).attr("title"));
             }
-		}
-		
-		$("#" + id + " a.photo").each(function(i) {
-			var imgsrc = $(this).attr("href").replace(/^i[/]/,"");
-			$(this).find("span.photo").css("background-image", "url('thumbs/" + imgsrc + "')")
-		});
-		$("#" + id +" .block h3").each(function(i) {
-			var date_parts = /^d(\d\d\d\d)-(\d\d)-(\d\d)$/.exec($(this).attr("id"));
-			if (date_parts.length > 0) {
-				var cmpd = new Date(Date.UTC(parseInt(date_parts[1], 10), parseInt(date_parts[2], 10) - 1, parseInt(date_parts[3], 10), 23, 59, 59));
-				if (show_age) $(this).attr("title", getDateDiffStr(epochd, cmpd, "date"));
-				if (show_age) $(this).find("span.age").html(getDateDiffStr(epochd, cmpd, "date_short"));
-			}
-		});
-		
-		$("#" + id).show();
-	}
-	
-	return false;
-}
+            var next_month = $("#" + id).next();
+            if (next_month.length == 0) {
+                $(".month_nav span.next").css("visibility", "hidden");
+                $(".month_nav span.last").css("visibility", "hidden");
+                if (curr_month != null && history != null && typeof history.pushState == 'function') {
+                    var filters = vlumi.Gallery.get_url_filters();
+                    history.pushState(null, 'Lenni', window.location.pathname + (filters.length > 0 ? '?' + filters : ''));
+                }
+            }
+            else {
+                $(".month_nav span.next").css("visibility", "").attr("title", $(next_month).attr("title"));
+                $(".month_nav span.last").css("visibility", "");
+                if (curr_month != null && history != null && typeof history.pushState == 'function') {
+                    var filters = vlumi.Gallery.get_url_filters();
+                    history.pushState(null, 'Lenni', window.location.pathname + '?m=' + id.substr(1) + (filters.length > 0 ? '&' + filters : ''));
+                }
+            }
+            
+            $("#" + id + " a.photo").each(function(i) {
+                var imgsrc = $(this).attr("href").replace(/^i\//,"");
+                $(this).find("span.photo").css("background-image", "url('thumbs/" + imgsrc + "')")
+            });
+            $("#" + id +" .block h3").each(function(i) {
+                var date_parts = /^d(\d\d\d\d)-(\d\d)-(\d\d)$/.exec($(this).attr("id"));
+                if (date_parts.length > 0) {
+                    var cmpd = new Date(Date.UTC(parseInt(date_parts[1], 10), parseInt(date_parts[2], 10) - 1, parseInt(date_parts[3], 10), 23, 59, 59));
+                    if (show_age) $(this).attr("title", vlumi.Age.getDateDiffStr(epochd, cmpd, "date"));
+                    if (show_age) $(this).find("span.age").html(vlumi.Age.getDateDiffStr(epochd, cmpd, "date_short"));
+                }
+            });
+            
+            $("#" + id).show();
+        }
+        
+        return false;
+    },
 
-function is_last_month() {
-    return $("div.month_block:visible").next().length == 0;
-}
+    is_last_month: function() {
+        return $("div.month_block:visible").next().length == 0;
+    },
 
-function get_curr_month() {
-    var item = $("div.month_block:visible");
-    if (item.length > 0) {
-	   return item.attr("id").substr(1);
-    }
-    else {
-        return null;
-    }
-}
+    get_curr_month: function() {
+        var item = $("div.month_block:visible");
+        if (item.length > 0) {
+            return item.attr("id").substr(1);
+        }
+        else {
+            return null;
+        }
+    },
 
-function show_prev_month() {
-	var item = $("div.month_block:visible").prev();
-	if (item.length > 0) {
-	   return show_month(item.attr("id"));
-	}
-	return false;
-}
+    show_prev_month: function() {
+        var item = $("div.month_block:visible").prev();
+        if (item.length > 0) {
+            return vlumi.Gallery.show_month(item.attr("id"));
+        }
+        return false;
+    },
 
-function show_next_month() {
-	var item = $("div.month_block:visible").next();
-	if (item.length > 0) {
-	   return show_month(item.attr("id"));
-	}
-	return false;
-}
+    show_next_month: function() {
+        var item = $("div.month_block:visible").next();
+        if (item.length > 0) {
+            return vlumi.Gallery.show_month(item.attr("id"));
+        }
+        return false;
+    },
 
-function show_month_containing(id) {
-	var month = get_month_containing(id);
-	if (month != null) {
-		show_month(month);
-	}
-	
-	return false;
-}
+    show_month_containing: function(id) {
+        var month = vlumi.Gallery.get_month_containing(id);
+        if (month != null) {
+            vlumi.Gallery.show_month(month);
+        }
+        
+        return false;
+    },
 
-function get_month_containing(id) {
-	return $("#" + id).parent().parent().attr("id");
-}
+    get_month_containing: function(id) {
+        return $("#" + id).parent().parent().attr("id");
+    },
 
-function get_url_filters() {
-	var filters = [];
-	
-	var url = $.url();
-	if (url.param('country') != null) {
-		filters.push('country=' + encodeURIComponent(url.param('country')).replace(/%20/g, '+'));
-	}
-	if (url.param('camera') != null) {
-		filters.push('camera=' + encodeURIComponent(url.param('camera')).replace(/%20/g, '+'));
-	}
-	if (url.param('author') != null) {
-		filters.push('author=' + encodeURIComponent(url.param('author')).replace(/%20/g, '+'));
-	}
-	
-	return filters.join("&");
-}
+    get_url_filters: function() {
+        var filters = [];
+        
+        var url = $.url();
+        if (url.param('country') != null) {
+            filters.push('country=' + encodeURIComponent(url.param('country')).replace(/%20/g, '+'));
+        }
+        if (url.param('camera') != null) {
+            filters.push('camera=' + encodeURIComponent(url.param('camera')).replace(/%20/g, '+'));
+        }
+        if (url.param('author') != null) {
+            filters.push('author=' + encodeURIComponent(url.param('author')).replace(/%20/g, '+'));
+        }
+        
+        return filters.join("&");
+    },
+};
